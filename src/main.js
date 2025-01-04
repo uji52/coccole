15d0487d583560ebad33949c83cae772c796fe64
uji52 <uji.52724@gmail.com>
2025-01-04 15:58:47 +0900

    CSS移動

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
-import "./assets/css/animate.css"
-import "./assets/css/bootstrap.css"
-import "./assets/css/bootstrap-datetimepicker.min.css"
-//import "./assets/css/flexslider.css"
-import "./assets/css/icomoon.css"
-import "./assets/css/simple-line-icons.css"
-import "./assets/css/style.css"
+import "@/assets/css/animate.css"
+import "@/assets/css/bootstrap.css"
+import "@/assets/css/bootstrap-datetimepicker.min.css"
+import "@/assets/css/flexslider.css"
+import "@/assets/css/icomoon.css"
+import "@/assets/css/simple-line-icons.css"
+import '@/assets/css/style.css'
 //<script src="js/modernizr-2.6.2.min.js"></script>
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLeaf } from '@fortawesome/free-solid-svg-icons'
