5c289048b58396b10f51030d6164834e8eacddbf
uji52 <uji.52724@gmail.com>
2025-01-04 12:30:45 +0900

    初期テストコード追加

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jsdom',
+}
