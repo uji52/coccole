7c81a3651b873ecf27d5ff5683a5fbb1b6114095
uji52 <uji.52724@gmail.com>
2025-02-02 14:18:18 +0900

    vite化推進

--- vite.config.js
+++ vite.config.js
@@ -2,67 +2,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
 import viteCompression from 'vite-plugin-compression'
-import { imagetools } from 'vite-imagetools'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz',
-      threshold: 10240,
-      deleteOriginFile: true
-    }),
-    imagetools(),
-    process.env.ANALYZE ? visualizer({ open: true }) : null
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    target: 'esnext',
-    minify: 'terser',
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['vue'],
-          'style': ['bootstrap']
-        }
+export default defineConfig(async () => {
+  const { imagetools } = await import('vite-imagetools');
+
+  return {
+    plugins: [
+      vue(),
+      viteCompression({
+        algorithm: 'brotli',
+        ext: '.br',
+        threshold: 10240,
+        deleteOriginFile: false
+      }),
+      imagetools(),
+      process.env.ANALYZE ? visualizer({ open: true }) : null
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
-      plugins: [
-        require('rollup-plugin-image-files')({
-          include: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
-          exclude: 'node_modules/**',
-          options: {
-            mozjpeg: {
-              progressive: true,
-              quality: 75,
-              arithmetic: false,
-            },
-            optipng: {
-              enabled: true,
-              optimizationLevel: 7,
-            },
-            pngquant: {
-              quality: [0.65, 0.90],
-              speed: 3,
-            },
-            webp: {
-              quality: 75,
-              lossless: false,
-              nearLossless: false,
-            }
+    },
+    build: {
+      target: 'esnext',
+      minify: 'terser',
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vendor': ['vue', '@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/vue-fontawesome'],
+            'style': ['bootstrap']
           }
-        })
-      ]
+        },
+      }
     }
-  },
-  server: {
-    hmr: true,
-    cors: true
-  }
-})
+  };
+});
