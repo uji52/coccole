dca82b3045d1fe50167db85f8fc01ecfab618283
uji52 <uji.52724@gmail.com>
2025-02-09 17:04:05 +0900

    現状の指定を縮小

--- vite.config.js
+++ vite.config.js
@@ -54,7 +54,7 @@ export default defineConfig(async () => {
             'fontawesome': ['@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/vue-fontawesome'],
             'style': ['bootstrap'],
             'utils': ['moment', 'axios'],
-            'jquery': ['jquery', 'jquery.easing', 'jquery.flexslider', 'jquery.stellar', 'jquery.waypoints'],
+            'jquery': ['jquery'],
           },
         },
       },
