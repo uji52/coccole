98b43e69750c7c47bd0b70df04de0448942a44a1
uji52 <uji.52724@gmail.com>
2025-08-17 15:23:42 +0900

    2025/09,10追加

--- src/components/CoccoleMain.vue
+++ src/components/CoccoleMain.vue
@@ -75,19 +75,28 @@
               営業スケジュールは下記の予定です。
             </p>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-4">
             <figure>
               <img
-                src="@/assets/images/schedule/202507.jpg"
+                src="@/assets/images/schedule/202508.jpg"
                 class="img-responsive"
                 alt="Free HTML5 Templates by FREEHTML5.co"
               >
             </figure>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-4">
             <figure>
               <img
-                src="@/assets/images/schedule/202508.jpg"
+                src="@/assets/images/schedule/202509.jpg"
+                class="img-responsive"
+                alt="Free HTML5 Templates by FREEHTML5.co"
+              >
+            </figure>
+          </div>
+          <div class="col-md-4">
+            <figure>
+              <img
+                src="@/assets/images/schedule/202510.jpg"
                 class="img-responsive"
                 alt="Free HTML5 Templates by FREEHTML5.co"
               >
