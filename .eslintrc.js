f2ce432335ec9ecb5931d0661dba3542c4e9ef35
uji52 <uji.52724@gmail.com>
2026-04-30 23:23:14 +0900

    Downgrade eslint-plugin-vue to 9.28.0 for ESLint 8.x compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,6 @@ module.exports = {
     {
       files: ['**/*.vue'],
       parser: 'vue-eslint-parser',
-      plugins: ['vue'],
       extends: ['plugin:vue/vue3-essential'],
       parserOptions: {
         parser: '@babel/eslint-parser',
